500b3ebf3a429b93e8f1aca5e7e5f4594eec1225
Vasyl Semak <vasylsmk1401@gmail.com>
2020-11-18 18:49:23 -0500

    initial commit

---
+++ client/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Riddle extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showAnswer: false,
+    }
+    this.toggleAnswer = this.toggleAnswer.bind(this)
+  }
+  toggleAnswer() {
+    this.setState(prevState => ({ showAnswer: !prevState.showAnswer }))
+  }
+  render() {
+    const { showAnswer } = this.state
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <p>A box without hinges, key, or lid,</p>
+        <p>yet golden treasure inside is hid.</p>
+        <p style={{ fontStyle: 'italic' }}>What am I?</p>
+        <button type="button" onClick={this.toggleAnswer}>
+          {showAnswer ? 'HIDE' : 'SHOW'} ANSWER
+        </button>
+        {showAnswer ? <p>🥚 AN EGG 🍳</p> : null}
+      </div>
+    )
+  }
+}
