f4c32b726fc345271a8d5f84058457e17bd1335b
Vasyl Semak <vasylsmk1401@gmail.com>
2020-11-19 21:48:28 -0500

    edit: fill Analitic comp with data from state

--- client/Analytics.js
+++ client/Analytics.js
@@ -1,25 +1,33 @@
 import React from 'react'
 
-export default ({ exercises }) => (
-  <div id="analytics">
-    <div id="analytics-content">
-      <div id="analytics-header">
-        <h2>Analytics</h2>
+export default ({ workouts }) => {
+  const allExercises = workouts.reduce((accum, w) => [...accum, ...w.exercises], [])
+  const completed = allExercises.filter(e => e.completed === true)
+  const totalMinutes = completed.reduce((accum, e) => (accum + e.duration), 0)
+  const percentage = completed.length / allExercises.length * 100
+
+  return (
+    <div id="analytics">
+      <div id="analytics-content">
+        <div id="analytics-header">
+          <h2>Analytics</h2>
+        </div>
+        <dl id="analytics-list">
+          <div>
+            <dt>Total Minutes Exercised:</dt>
+            <dd id="total-minutes">{totalMinutes}</dd>
+          </div>
+          <div>
+            <dt>Favorite Exercise:</dt>
+            <dd>Running</dd>
+          </div>
+          <div>
+            <dt>Percentage Completed:</dt>
+            <dd id="percentage-completed">{`${percentage}%`}</dd>
+          </div>
+        </dl>
       </div>
-      <dl id="analytics-list">
-        <div>
-          <dt>Total Minutes Exercised:</dt>
-          <dd id="total-minutes">95</dd>
-        </div>
-        <div>
-          <dt>Favorite Exercise:</dt>
-          <dd>Running</dd>
-        </div>
-        <div>
-          <dt>Percentage Completed:</dt>
-          <dd id="percentage-completed">75%</dd>
-        </div>
-      </dl>
     </div>
-  </div>
-)
+  )
+}
+
